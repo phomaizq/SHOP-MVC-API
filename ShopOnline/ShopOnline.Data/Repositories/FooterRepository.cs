675ff49a3b81d6daf0dd2a8dcad61b40cb8d5d5a
Phomai <nguyentinhoaian@gmail.com>
2024-08-12 13:43:08 +0700

    commit

---
+++ ShopOnline/ShopOnline.Data/Repositories/FooterRepository.cs
@@ -0,0 +1,16 @@
+﻿using ShopOnline.Data.Infrastructure;
+using ShopOnline.Model.Models;
+
+namespace ShopOnline.Data.Repositories
+{
+    public interface IFooterRepository : IRepository<Footer>
+    {
+    }
+
+    public class FooterRepository : RepositoryBase<Footer>, IFooterRepository
+    {
+        public FooterRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
